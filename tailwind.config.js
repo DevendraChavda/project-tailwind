f3c5496cc6a7babdec80ca7dab62b4f32855e0b9
Himanshu Borah <borahhimanshu3@gmail.com>
2023-03-28 16:46:56 +0530

    ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
